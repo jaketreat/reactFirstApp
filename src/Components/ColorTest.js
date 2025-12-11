93305a6b0f67b02e89be227f105dc3bef054ead9
Jake Treat <jaketreat@yahoo.com>
2025-12-11 14:48:45 -0600

    cleaned up some logic in the test component.

--- src/Components/ColorTest.js
+++ src/Components/ColorTest.js
@@ -3,10 +3,17 @@ import { useState } from "react";
 function ColorTest() {
     const [greenOrWhite] = useState(() => Math.floor(Math.random() * 10));
     console.log(greenOrWhite);
+    
+    const colorInfo = {
+        green: { answer: "Green", className: "greenHead" },
+        white: { answer: "White", className: "whiteHead"}
+    }
+
+    const choice = greenOrWhite >= 5 ? "green" : "white";
 
     return (
         <div>
-            <h2>{greenOrWhite > 5 ? "Yes" : "No"}</h2>
+            <h2 className="whiteHead">Click the <span className={colorInfo[choice].className}>{colorInfo[choice].answer}</span> logo.</h2>
         </div>
     )
 }
